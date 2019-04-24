a04d6f1d9afcbe5c3cd96f0295dc8d6f5157de06
Sam Waters <samwatersw15@gmail.com>
2019-04-24 07:36:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (var i = 1; i <= 25; i++) {
        if (i === 1) {
-         console.log(`I am ${i} loop.`)
+         array.push(`I am ${i} strange loop.`)
        }
        else {
-         console.log(`I am ${i} loops.`)
+         array.push(`I am ${i} strange loops.`)
        }
   }
 }
